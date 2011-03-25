46a7944c67e21a36c3b9834a61cd0d7859532b37
natanabolafya <natanabolafya@0dce60d2-7524-ee88-523d-13e24cf442db>
2011-03-25 00:47:02 +0000

    winner select

---
+++ SubscriptionClient/src/client/Configure.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ *
+ * @author CUNEYT
+ */
+public class Configure {
+
+    private Properties prop;
+
+    /**
+     * The configuration  class to read the configurations from the properties file.
+     * @throws IOException
+     */
+    public Configure() throws IOException {
+        prop = new Properties();
+        prop.load(this.getClass().getClassLoader().getResourceAsStream("client/client.properties"));
+    }
+
+    /**
+     * Gets the specified property value associated with the given key or null
+     * if the key does not exist.
+     * @param key the keyword of the property
+     * @return the value of the specified key
+     */
+    public String getProperty(String key) {
+        return prop.getProperty(key);
+    }
+}
