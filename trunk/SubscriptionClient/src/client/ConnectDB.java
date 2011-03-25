9f717bdad6003cbaa930951c4bed49ab7ff515bc
natanabolafya <natanabolafya@0dce60d2-7524-ee88-523d-13e24cf442db>
2011-03-25 01:16:12 +0000

    winner select

--- trunk/SubscriptionClient/src/client/ConnectDB.java
+++ trunk/SubscriptionClient/src/client/ConnectDB.java
@@ -1,39 +1,32 @@
 package client;
 
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectDB {
-	private static Connection connection;
-	
-	public ConnectDB() {
-		newConnection();
-	}
-	
-	public static void newConnection(){
-		connection = null;
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String username = "okuyucu";
-		String password = "oku123yucu";
-		String url = "jdbc:mysql://mysql02.natro.com:3306/orenux";
-		try {
-			connection = DriverManager.getConnection(url, username, password);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println("connection problem");
-			e.printStackTrace();
-		}
-	}
-	public static Connection getConnection(){
-		return connection;
-	}
 
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            String username = "okuyucu";
+            String password = "oku123yucu";
+            String url = "jdbc:mysql://mysql02.natro.com:3306/orenux";
+            try {
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                System.out.println("connection problem");
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
 }
---
+++ trunk/SubscriptionClient/src/client/Ponger.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package client;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Natan
+ */
+public class Ponger implements Runnable {
+
+    private int port;
+
+    public Ponger(int port) {
+        this.port = port;
+    }
+
+    public void run() {
+        try {
+            ServerSocket ser = new ServerSocket(port);
+            Socket sock = ser.accept();
+            sock.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Ponger.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
